1cf3828271a890f36487323e088ff029dc1e7116
Frangithub90 <fco.manuel.candela90@gmail.com>
2026-05-31 01:33:52 +0200

    build: initialize spring boot project, pom.xml, and folder structure

---
+++ src/main/java/com/ironhack/smartqr/SmartQrApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.smartqr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartQrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartQrApplication.class, args);
+    }
+
+}
